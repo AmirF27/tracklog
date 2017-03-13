772be52aa08389cef6b7c504c07c74a5afd73f91
Amir Fakheraldeen <amir.f.aldeen@gmail.com>
2017-03-13 14:22:58 +0200

    Add ability to clear game selection

--- static/js/script.js
+++ static/js/script.js
@@ -1,16 +1,25 @@
 var removeIcon = " <i class='fa fa-times' aria-hidden='true'></i>";
+var xhrPool = [];
 
 $(function() {
     $("#game-search").on("input", function() {
-        search($(this).val());
+        if ($(this).val()) {
+            search($(this).val());
+        }
+        else {
+            $(".search-results").remove();
+            xhrPool.forEach(function(xhr) {
+                xhr.abort();
+            });
+            xhrPool = [];
+        }
     });
-
-    
 });
 
 function search(query) {
-    $.getJSON(Flask.url_for("search"), { q: query })
-    .then(function(data) {
+    // http://www.ajaxload.info/
+    $(".game-results").html("<img src='/static/img/ajax-loader.gif'>");
+    xhr = $.getJSON(Flask.url_for("search"), { q: query }, function(data) {
         var results = [];
         data.forEach(function(game) {
             results.push(
@@ -21,22 +30,39 @@ function search(query) {
         });
         $(".game-results").html(
             $("<ul/>", {
-                "class": "list-group",
+                "class": "list-group search-results",
                 html: results.join("")
             })
         );
 
-        $("li").on("click", function() {
-            chooseGame({
+        $(".search-results li").on("click", function() {
+            setGameSelection({
                 id: $(this).data("game-id"),
                 name: $("span", this).text()
             });
         });
     });
+
+    xhrPool.push(xhr);
 }
 
-function chooseGame(game) {
-    $("#game-search").hide();
+function setGameSelection(game) {
+    xhrPool.forEach(function(xhr) {
+        xhr.abort();
+    });
+    xhrPool = [];
+
+    $("#game-search").val("").hide();
     $(".game-panel").show();
     $(".game-panel .panel-body").html(game.name + removeIcon).attr("data-game-id", game.id);
+    $(".search-results").remove();
+
+    $(".game-panel .panel-body .fa-times").on("click", function() {
+        clearGameSelection();
+    });
+}
+
+function clearGameSelection() {
+    $(".game-panel").hide();
+    $("#game-search").show().focus();
 }
